c7510ce1926e1bed4765d42e6de8d20788b70bbb
Alvaro Maceda <alvaro@alvaromaceda.es>
2018-02-18 09:28:34 +0100

    Funcionamiento de la pantalla

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var secuencia = [
       {item:'blue',salida:['red','blue','yellow']},
-      {item:'blue',salida:'red'},
-      {item:'blue',salida:'red'},
-      {item:'blue',salida:'red'},
-      {item:'blue',salida:'red'},
-      {item:'blue',salida:'red'},
-      {item:'blue',salida:'red'},
+      {item:'blue',salida:coloresAleatorios},
+      {item:'blue',salida:coloresAleatorios},
+      {item:'blue',salida:coloresAleatorios},
+      {item:'blue',salida:['red','blue','yellow']},
+      {item:'blue',salida:['red','blue','yellow']},
+      {item:'blue',salida:['green','green','green']},
 ];
+
+// Niños no hagáis esto en proyectos de verdad
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+};
+
+var colores = ['red','blue','magenta','red','yellow'];
+function coloresAleatorios() {
+    return [colores.randomElement(),colores.randomElement(),colores.randomElement()];
+}
 
 var estado = new EstadoNormal(secuencia)
     .alIterar(ponerEstadoPantalla)
@@ -14,30 +24,42 @@ var estado = new EstadoNormal(secuencia)
     .alPerder(ponerPantallaEnEstadoPerdedor);
 
 function instalarManejadores() {
-    document.querySelectorAll('.boton').forEach(function(boton){
-        boton.addEventListener("click", function(event){
-            estado = estado.next(event.target.dataset.color)
+
+    function instalarManejadoresColores(){
+        document.querySelectorAll('.boton-color').forEach(function(boton){
+            boton.addEventListener("click", function(event){
+                estado = estado.next(event.target.dataset.color)
+            });
         });
-    });
+    }
+
+    function instalarManejadorReintentar() {
+        document.querySelector('#reintentar')
+            .addEventListener("click",function() {
+                location.reload();
+            });
+    }
+
+    instalarManejadoresColores();
+    instalarManejadorReintentar();
 }
 
-function ponerEstadoPantalla(estado) {
-    console.log('iterar:'+estado);
+function ponerEstadoPantalla(estados) {
+    if(estados instanceof Function) estados = estados();
     var i=0;
     document.querySelectorAll('[id^=estado-]').forEach(function(item){
-        console.log(item);
-        console.log(estado[i]);
-        item.style['background-color'] = estado[i++];
+        item.style['background-color'] = estados[i++];
     });
 }
 function ponerPantallaEnEstadoGanador() {
-    console.log('ganar');
+    document.querySelector('#ganar').style['display'] = 'block';
 }
 function ponerPantallaEnEstadoPerdedor() {
-    console.log('perder');
+    document.querySelector('#perder').style['display'] = 'block';
 }
 
-
 window.onload = function() {
+    const estadoInicial = ['black','black','black'];
     instalarManejadores();
+    ponerEstadoPantalla(estadoInicial);
 };
